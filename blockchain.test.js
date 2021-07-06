a9893d65a777ec3900bc93876e35c82f0c28ac7d
Patrick Ongeche <daltonongeche@gmail.com>
2021-07-06 16:52:59 +0300

    errormock and logmock

--- blockchain.test.js
+++ blockchain.test.js
@@ -64,19 +64,34 @@ describe('Blockchain', () => {
                 it('returns true', () => {
                     expect(Blockchain.isValidChain(blockchain.chain)).toBe(true);
                 });
-                
+               
             });
         });
     });
 
     describe('replaceChain()', () => {
-         describe('when the new chain is not longer', () =>{
-             it('does not replace the chain', () =>{
+
+        let errorMock, logMock;
+
+        beforeEach(() => {
+            errorMock = jest.fn();
+            logMock = jest.fn();
+
+            global.console.error = errorMock;
+            global.console.log = logMock
+        })
+         describe('when the new chain is not longer', () => {
+             beforeEach(() => {
                 newChain.chain[0] = {new: 'chain'};
 
-               blockchain.replaceChain(newChain.chain);
-
+                blockchain.replaceChain(newChain.chain);
+             })
+             it('does not replace the chain', () =>{
                expect(blockchain.chain).toEqual(originalChain);
+             });
+
+             it('logs an error', () => {
+                 expect(errorMock).toHaveBeenCalled();
              });
          });
 
@@ -88,12 +103,17 @@ describe('Blockchain', () => {
             });
 
              describe('and the chain is invalid', () => {
+                 beforeEach(() => {
+                      newChain.chain[2].hash = 'some-fake-hash';
+
+                      blockchain.replaceChain(newChain.chain);
+                 })
                  it('does not replace the chain', () => {
-                     newChain.chain[2].hash = 'some-fake-hash';
+                    expect(blockchain.chain).toEqual(originalChain);
+                 });
 
-                     blockchain.replaceChain(newChain.chain); 
-
-                     expect(blockchain.chain).toEqual(originalChain);
+                 it('logs an error', () => {
+                     expect(errorMock).toHaveBeenCalled();
                  })
              });
 
@@ -102,7 +122,11 @@ describe('Blockchain', () => {
                       blockchain.replaceChain(newChain.chain);
 
                       expect(blockchain.chain).toEqual(newChain.chain);
-                 })
+                 });
+
+                 it('logs about the chain replacement', () => {
+                    expect(errorMock).toHaveBeenCalled();
+                 }) 
              })
          })
     });
