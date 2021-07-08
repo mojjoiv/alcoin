436c4b19d852979905421480059cefe94e2e79c0
Patrick Ongeche <daltonongeche@gmail.com>
2021-07-08 15:57:50 +0300

    dynamic difficulty and mine rate

--- block.js
+++ block.js
@@ -1,4 +1,4 @@
-const { GENESIS_DATA } = require('./config');
+const { GENESIS_DATA, MINE_RATE } = require('./config');
 const cryptoHash = require('./crypto-hash');
 
 class Block {
@@ -38,6 +38,17 @@ class Block {
             nonce,
             hash
         });
+
+    }
+
+    static adjustDifficulty({ originalBlock, timestamp }) {
+        const { difficulty } = originalBlock;
+
+        const difference = timestamp - originalBlock.timestamp;
+
+        if (difference > MINE_RATE) return difficulty - 1;
+
+        return difficulty + 1;
     }
 }
 
